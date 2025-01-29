129e15969dabc7bc0d9b32c7e348d2d644215539
Izzamuddin Royhul Firdaus <dimanton1221@gmail.com>
2025-01-29 17:28:58 +0700

    init commit

---
+++ entities/struct.go
@@ -0,0 +1,12 @@
+package entities
+
+type Hehe struct {
+	Model    string    `json:"model"`
+	Messages []Message `json:"messages"`
+	Stream   bool      `json:"stream"`
+}
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
